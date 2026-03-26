3e092b3cbf691d1b505cb1f90bf281607956d7a9
LeonardoElish <2997462324@qq.com>
2026-03-26 15:59:17 +0800

    modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -21,14 +21,7 @@ export default defineConfig({
     }
   },
   build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return "vendor";
-          }
-        }
-      }
-    }
+    minify: true,
+    reportCompressedSize: false
   }
 });
